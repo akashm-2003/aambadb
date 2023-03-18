f2046e96527ea1c639d925b5db40cc351b12cd62
Akash Manna <akashmanna318@gmail.com>
2023-03-18 08:41:32 +0530

    Login Added variables

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ const signInInterface = document.getElementById("signIn");
 const container = document.getElementById("container");
 const signUp = document.getElementById("signUpClick");
 const signIn = document.getElementById("signInClick");
-
+const newName = document.getElementById("newName");
+const newEmail = document.getElementById("newEmail");
+const newPass = document.getElementById("newPass");
+const oldEmail = document.getElementById("oldEmail");
+const oldPass = document.getElementById("oldPass");
 signUpInterface.addEventListener("click", () => {
   container.classList.add("right-panel-active");
 });
